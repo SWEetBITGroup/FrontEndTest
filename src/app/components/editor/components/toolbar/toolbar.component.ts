eb70e57aefd5827c3ac6b2562e216bae3c1925f3
Giansalmi <gianmarco.salmistraro.1@studenti.unipd.it>
2017-07-04 17:25:41 +0200

    aggiunti commenti alle funzioni, definite solo in locale da me

--- src/app/components/editor/components/toolbar/toolbar.component.ts
+++ src/app/components/editor/components/toolbar/toolbar.component.ts
@@ -12,12 +12,46 @@ export class ToolbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Method add class to editor
+   */
   addClasse() {}
+
+  /**
+   * Method add interface to editor
+   */
   addInterfaccia() {}
+
+  /**
+   * Method add abstract class to editor
+   */
   addAstratta() {}
+
+  /**
+   * Method selects association as connector
+   */
   addAssociazione() {}
+
+  /**
+   * Method selects implementation as connector
+   */
   addImplementazione() {}
+
+  /**
+   * Method selects generalization as connector
+   */
   addGeneralizzazione() {}
+
+  /**
+   * Method add comment cell to editor
+   */
   addCommento() {}
+
+  /**
+   * Method add selected connector to editor if target element is selected, else the method selects the source element
+   * @param cellView 
+   * Source or target element
+   */
+  addConnettore(cellView: any) {}
 
 }
