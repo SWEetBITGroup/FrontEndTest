bda9105a584df080a4b3a1d4259167e05f610f5c
Firion711 <jacknapier.serious@gmail.com>
2017-07-06 12:55:57 +0200

    ADD commenti a metodo.ts

--- src/app/components/editor/models/metodo.ts
+++ src/app/components/editor/models/metodo.ts
@@ -2,13 +2,38 @@ import { Param } from './param';
 
 import * as joint from 'jointjs';
 
+  /**
+   * it define the class's method
+   */
 export class Metodo {
+    /**
+     * it define the method's name
+     */
     nome: string;
+    /**
+     * it define the method's visibility
+     */
     accesso: string;
+    /**
+     * it define the method's return type
+     */
     tipoRitorno: string;
+    /**
+     * it define the list of formal parameters by an array
+     */
     listaArgomenti: Param[];
+    /**
+     * it define the current method into JSON format
+     */
     diagramma: JSON;
 
+    /**
+     * it make a Metodo object, listaArg is optional
+     * @param nome
+     * @param acc
+     * @param tipo
+     * @param listaArg
+     */
     constructor(nome: string, acc: string, tipo: string, listaArg?: Param[]) {
         this.nome = nome;
         this.accesso = acc;
@@ -18,48 +43,87 @@ export class Metodo {
           this.listaArgomenti = listaArg;
     }
 
+    /**
+     * it change the method's name
+     * @param nome
+     */
     changeNome(name: string) {
         this.nome = name;
     }
 
+    /**
+     * it change the method's return type
+     * @param tipo
+     */
     changeTipoRitorno(tipo: string) {
         this.tipoRitorno = tipo;
     }
 
+    /**
+     * it change the method's visibility
+     * @param acc
+     */
     changeAccesso(acc: string) {
         this.accesso = acc;
     }
 
+    /**
+     * it change the reference of the formal paremeters array
+     * @param listaArg
+     */
     changeListaArg(listArg: Param[]) {
         this.listaArgomenti = listArg;
     }
 
+    /**
+     * it append to listaArgomenti a new parameter
+     * @param arg
+     */
     addArgomento(arg: Param) {
         if(!this.listaArgomenti)
             this.listaArgomenti = new Array<Param>();
         this.listaArgomenti.push(arg);
     }
 
+    /**
+     * it assigns to diagramma attributes class, the JSON file
+     * @param dia
+     */
     addDiagram(dia: JSON) {
         this.diagramma = dia;
     }
 
+    /**
+     * it returns the method's diagramma
+     */
     getDiagram() {
         return this.diagramma;
     }
 
+    /**
+     * it returns the method's nome
+     */
     getNome() {
         return this.nome;
     }
 
+    /**
+     * it returns the method's visibility
+     */
     getAccesso() {
         return this.accesso;
     }
 
+    /**
+     * it returns the method's return type
+     */
     getTipoRitorno() {
         return this.tipoRitorno;
     }
 
+    /**
+     * it returns the method's parameters list
+     */
     getListaArgomenti() {
         return this.listaArgomenti;
     }
